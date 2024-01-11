c923429ad27bb0afc1473e7c2503e406aa5e8e27
khpeet <kpeet@newrelic.com>
2024-01-11 15:29:00 -0500

    increase nrql timeout

--- nerdlets/home/utils.js
+++ nerdlets/home/utils.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       actor {
         account(id: ${account}) {
-          nrql(query: "SELECT uniques(conditionId, 5000) as 'conditions' FROM NrAiSignal where numberOfDeviations is not null since 1 month ago") {
+          nrql(query: "SELECT uniques(conditionId, 5000) as 'conditions' FROM NrAiSignal where numberOfDeviations is not null since 1 month ago", timeout: 90) {
             results
           }
         }
